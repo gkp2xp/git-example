1bbe5a124912dff173e1e9819e6d7656ceb00b78
gakpoly <gakpoly@synergybis.com>
2021-04-30 19:35:18 -0400

    first commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/*
+ * Placed in the public domain by Bryan O'Sullivan.  This program is
+ * not covered by patents in the United States or other countries.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello, world!\");
+	return 0;
+}
